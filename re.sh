c63741dc10660a89695df9a5e681433be02c3704
paul <tuner777@yahoo.com>
2018-04-03 12:01:08 -0500

    Added the ability to specify virtual hosts and service command

--- re.sh
+++ re.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-
+CONFIG="$1"
+COMMAND="$2"
+if [ $# -ne 2 ]
+then
+    echo "$0 requires two paramters {virtual-host} {restart|reload}"
+    exit 1
+fi
 # Move the current execution state to the proper directory
 cd /etc/apache2/sites-available
 
